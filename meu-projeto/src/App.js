f0022e93e4d7728ff7bfcee588f5ebee5165404c
Rafael Brandão da Silva <113553591+RafaelBrandaoS@users.noreply.github.com>
2023-02-09 16:39:58 -0300

    instalei o React

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá React!</h1>
+      <p>minha primeira aplicação com React</p>
+    </div>
+  );
+}
+
+export default App;
