32d20cab25423c7e3184438c2706b2f485c159e7
Rafael Brandão da Silva <113553591+RafaelBrandaoS@users.noreply.github.com>
2023-02-22 13:44:25 -0300

    state list

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,18 +1,20 @@
+import {useState} from 'react'
 import './App.css'
-import OutraLista from './components/OutraLista'
+import Saudacao from './components/Saudacao'
+import SeuNome from './components/SeuNome'
 
 function App() {
 
-  const meusItens = ['html', 'css', 'javaScript']
-  const maisItens = []
+  const [nome, setNome] = useState()
 
   return (
     <div className="App">
-      <h1>Renderização de Listas</h1>
-      <OutraLista itens={meusItens} />
-      <OutraLista itens={maisItens}/>
+      <h1>State lift</h1>
+      <SeuNome setNome={setNome} />
+      <Saudacao nome={nome} />
     </div>
   )
 }
 
 export default App
+ 
